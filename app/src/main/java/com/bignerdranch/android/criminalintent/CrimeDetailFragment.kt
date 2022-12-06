a11259a6a0b5197c4cf8e55fb95591fc1a1400bf
unknown <alumno@valles-lab06-36.academicos.valles.udg.mx>
2022-12-06 10:18:31 -0600

    inicio de repositorio

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,51 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.*
+
+class CrimeDetailFragment : Fragment() {
+    private var _binding: FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    private val args: CrimeDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding =
+            FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+            }
+            crimeDate.apply {
+                isEnabled = false
+            }
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+            }
+
+
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
